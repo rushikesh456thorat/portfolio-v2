a6224b06a28888797e76b8dbe8ccaa9adb172ceb
Rushikesh Thorat <80233861+rushikesh456thorat@users.noreply.github.com>
2026-09-23 12:05:16 +0530

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/",
+  base:"/portfolio-v2/",
   plugins: [react(),tailwindcss(),]
 })
